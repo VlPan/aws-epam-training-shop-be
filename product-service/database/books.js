161596f997884471d42282800af889d7f6dfcccb
UPantsiukhou <UPantsiukhou@slb.com>
2023-03-12 15:40:45 +0100

    feat: task-3

---
+++ product-service/database/books.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id: '1',
+        name: 'Book1',
+        price: 1
+    },
+    {
+        id: '2',
+        name: 'Book2',
+        price: 2
+    },
+    {
+        id: '3',
+        name: 'Book3',
+        price: 3
+    },
+]
